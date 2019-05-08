6bbab1222c7969a2fa0084a9527448291b4e7794
XinPing Wang <xinping.wang@hotmail.com>
2019-05-08 20:22:35 +0800

    top news paging

--- src/views/TopNews.vue
+++ src/views/TopNews.vue
@@ -1,32 +1,68 @@
 <template>
-  <ul>
-    <li v-for="news in currentNewsList" :key="news">
-      <news-item :news-id="news"/>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li v-for="news in currentNewsList" :key="news">
+        <news-item :news-id="news"/>
+      </li>
+    </ul>
+    <div>
+      <button :disabled="currentPage === 0" @click="previousPage">Previous</button>
+      <button v-show="currentPage - 2 > 0" @click="gotoPage(currentPage - 2)">
+        {{currentPage - 2}}
+      </button>
+      <button v-show="currentPage - 1 > 0" @click="gotoPage(currentPage - 1)">
+        {{currentPage - 1}}
+      </button>
+      <span style="padding-left: 4px; padding-right: 4px; font-size:11px">
+        {{ currentPage }}
+      </span>
+      <button v-show="currentPage + 1 < totalPages" @click="gotoPage(currentPage + 1)">
+        {{currentPage + 1}}
+      </button>
+      <button v-show="currentPage + 2 < totalPages" @click="gotoPage(currentPage + 2)">
+        {{currentPage + 2}}
+      </button>
+      <button @click="nextPage">Next</button>
+    </div>
+  </div>
 </template>
 
 <script>
 const axios = require("axios");
-import NewsItem from "@/components/NewsItem.vue"
+import NewsItem from "@/components/NewsItem.vue";
 
 export default {
   name: "TopNews",
-  components: {NewsItem},
+  components: { NewsItem },
   data() {
     return {
       allNewsList: [],
-      currentPage: 0
+      currentPage: 1,
+      pageLimt: 10
     };
   },
   computed: {
+    totalPages: function() {
+      return this.allNewsList.length / this.pageLimt;
+    },
     currentNewsList: function() {
       return this.allNewsList.length > 0
         ? this.allNewsList.slice(
-            this.currentPage * 10,
-            (this.currentPage + 1) * 10
+            (this.currentPage - 1) * this.pageLimt,
+            this.currentPage * this.pageLimt
           )
         : [];
+    }
+  },
+  methods: {
+    previousPage: function() {
+      this.currentPage--;
+    },
+    nextPage: function() {
+      this.currentPage++;
+    },
+    gotoPage: function(target) {
+      this.currentPage = target;
     }
   },
   mounted() {
