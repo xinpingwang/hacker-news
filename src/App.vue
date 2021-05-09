bb0897f27a8ed4d80461db686aec48ad54184e7f
XinPing Wang <xinping.wang@hotmail.com>
2021-05-09 20:36:17 +0800

    feat: page layout style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <header>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href>Hacker News</a>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <div class="navbar-nav">
-            <router-link class="nav-item nav-link" :to="{ path: '/'}">Top</router-link>
-            <router-link class="nav-item nav-link" :to="{ path: '/new'}">New</router-link>
-          </div>
-        </div>
+    <header class="header-bar">
+      <router-link class="logo-text mr-12" :to="{ path: '/' }">
+        <h1>Hacker News</h1>
+      </router-link>
+      <nav class="nav-wrapper">
+        <router-link class="nav-item" :to="{ path: '/' }">Top</router-link>
+        <router-link class="nav-item" :to="{ path: '/new' }">New</router-link>
       </nav>
     </header>
-    <div class="container-fluid" style="padding-top: 26px">
+    <div class="content-wrapper" style="">
       <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: "app"
-};
+  export default {
+    name: "app",
+  };
 </script>
 
-<style>
+<style lang="postcss" scoped>
+  .header-bar {
+    @apply flex items-center bg-black text-white h-14 pl-8;
+  }
+
+  .logo-text {
+    @apply font-serif text-2xl text-gray-200;
+  }
+
+  .nav-wrapper {
+    @apply h-full;
+  }
+
+  .nav-item {
+    @apply inline-flex h-full items-center p-4 hover:bg-gray-800;
+  }
+
+  .nav-item.router-link-exact-active {
+    @apply bg-gray-800;
+  }
+
+  .content-wrapper {
+    @apply m-auto w-8/12 mt-16;
+  }
 </style>
